86a5c428a1f3132a1b97af8dfb83c51c6a41dfe3
Andriy <andremaxico321@gmail.com>
2025-05-03 18:48:20 +0300

    added footer component

---
+++ src/app/components/CountryLookup.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React, { useEffect, useState } from 'react'
+
+type PropsType = {};
+
+export const CountryLookup: React.FC<PropsType> = ({}) => {
+	const [country, setCountry] = useState<string>('United States');
+
+	const ipInfoRequestToken = process.env.NEXT_PUBLIC_IP_KEY;
+
+	console.log('ip info request token', ipInfoRequestToken);
+
+	useEffect(() => {
+		const getCountry = async () => {
+			// const ipRes = await fetch(`https://ipinfo.io/json?token=${ipInfoRequestToken}`);
+			// const ipData = await ipRes.json();
+			const response = await fetch(`https://extreme-ip-lookup.com/json/${ipInfoRequestToken}`);
+			const data = await response.json();
+			const country = data.country;
+			setCountry(country as string);
+		}
+		if(ipInfoRequestToken) {
+			getCountry(); 
+		}
+	}, [ipInfoRequestToken])
+
+	return (
+		<div className='
+			px-8 py-3 border-b-1
+		'>
+			{country}
+		</div>
+	)
+}
---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { CountryLookup } from './CountryLookup';
+
+type PropsType = {}
+
+export const Footer: React.FC<PropsType> = ({}) => {
+	return (
+		<footer className='
+			absolute bottom-0 text-sm text-gray-500 bg-[#fcfcfc] w-full
+		'>
+			<CountryLookup />
+			<nav className='
+				flex flex-col items-center 
+				px-8 py-3 space-y-3
+				sm:flex-row sm:justify-between
+				sm: space-y-0
+			'>
+				<ul className='
+					flex items-center space-x-5
+				'>
+					<li className='hover:underline cursor-pointer'>About</li>
+					<li className='hover:underline cursor-pointer'>Advertizing</li>
+					<li className='hover:underline cursor-pointer'>Business</li>
+					<li className='hover:underline cursor-pointer'>How Search works</li>
+				</ul>
+				<ul className='
+					flex items-center space-x-5
+				'>
+					<li className='hover:underline cursor-pointer'>Privacy</li>
+					<li className='hover:underline cursor-pointer'>Terms</li>
+					<li className='hover:underline cursor-pointer'>Setting</li>
+				</ul>
+			</nav>
+		</footer>
+	)
+}
