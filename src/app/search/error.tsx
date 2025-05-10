b523c6a9f719ff9f70b0026ba4a63bcd518c0f05
Andriy <andremaxico321@gmail.com>
2025-05-10 08:47:57 +0300

    handle possible errors

---
+++ src/app/search/error.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React, { useEffect } from 'react'
+
+type PropsType = {
+	error: Error,
+	reset: () => void,
+};
+
+function Error({error, reset}: PropsType) {
+	useEffect(() => {
+		console.log('error', error);
+	}, [error])
+
+	return (
+		<div className='flex flex-col justify-center items-center pt-10'>
+			<h1 className='text-3xl mb-4'>Something went wrong!</h1>
+			<button className='text-primary cursor-pointer'>Try again</button>
+		</div>
+	)
+}
+
+export default Error; 
--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,4 +1,5 @@
 import { SearchParams } from 'next/dist/server/request/search-params'
+import Link from 'next/link'
 import React from 'react'
 
 type PropsType = {
@@ -38,9 +39,13 @@ type ResultType = {
 }
 
 async function WebSearchPage({ searchParams }: PropsType) {
+	const searchTerm = (await searchParams).searchTerm;
+
 	const response = await fetch(`
-		https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}
+		https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}
 	`);
+
+	if(!response.ok) throw new Error('Something went wrong');
 
 	const data = await response.json();
 	const results = data.items as ResultType[];
@@ -50,6 +55,15 @@ async function WebSearchPage({ searchParams }: PropsType) {
 			{results && results.map(result => (
 				<p key={result.formattedUrl}>{result.title}</p>
 			))}
+			{(!results || results.length === 0) && 
+				<div className='flex flex-col items-center'>
+					<h1 className='text-3xl'>No results found for {searchTerm}</h1>
+					<p >
+						Try searching the web or images for something else
+					</p>
+					<Link href='' className='text-primary cursor-pointer'>Home</Link>
+				</div>
+			}
 		</div>
 	)
 }
